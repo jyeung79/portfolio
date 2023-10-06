4674a5d885db558508301a8e9ef7e289ef4b5e5b
jyeung79 <jyeung.eng@gmail.com>
2023-10-05 19:22:51 -0700

    Update next config files

--- next.config.js
+++ next.config.js
@@ -1,7 +1,7 @@
 const withNextra = require('nextra')({
   theme: 'nextra-theme-blog',
   themeConfig: './theme.config.js',
-  // optional: add `unstable_staticImage: true` to enable Nextra's auto image import
+  unstable_staticImage: true,
 })
 
 /** @type {import('next').NextConfig} */
--- theme.config.js
+++ theme.config.js
@@ -1,11 +1,17 @@
+import Link from "next/link"
 const YEAR = new Date().getFullYear()
 
 export default {
   footer: (
     <footer>
       <small>
-        <time>{YEAR}</time> © Your Name.
-        <a href="/feed.xml">RSS</a>
+        <time>{YEAR}</time> © Jeffery Yeung
+      </small>
+      <div />
+      <small>
+        Credit to <Link href="https://github.com/chronark/chronark.com">@chronark
+        </Link>, <Link href="https://github.com/shuding/nextra">@nextra Blog Theme
+        </Link>
       </small>
       <style jsx>{`
         footer {
