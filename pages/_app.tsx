86a5a0a9aae2bc72d7cdf010c35f9a612db48707
jyeung79 <jyeung.eng@gmail.com>
2023-10-05 19:23:50 -0700

    feat: Update portfolio to use particles and transitions

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -2,17 +2,12 @@ import 'nextra-theme-blog/style.css'
 import type { AppProps } from 'next/app'
 import Head from 'next/head'
 import '../styles/main.css'
+import Particles from '../components/particles'
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
     <>
       <Head>
-        <link
-          rel="alternate"
-          type="application/rss+xml"
-          title="RSS"
-          href="/feed.xml"
-        />
         <link
           rel="preload"
           href="/fonts/Inter-roman.latin.var.woff2"
@@ -21,6 +16,7 @@ export default function App({ Component, pageProps }: AppProps) {
           crossOrigin="anonymous"
         />
       </Head>
+      <Particles className="absolute inset-0 -z-10 animate-fade-in" quantity={100} />
       <Component {...pageProps} />
     </>
   )
